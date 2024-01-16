aafe9462e1ada717dca7ad0da45a53112d823918
Manu Muraleedharan <manu.digital@gmail.com>
2024-01-16 16:16:03 +0530

    t2 medium

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ locals {
 
 resource "aws_instance" "instance" {
   ami = "ami-090fa75af13c156b4"
-  instance_type = "t2.small"
+  instance_type = "t2.medium"
 
   tags = {
     project_name = var.project_name
